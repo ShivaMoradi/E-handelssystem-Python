9becbd78087851a29aa858aac187d10d5b5235f8
ShivaMoradi <shiva.moradi1@gmail.com>
2024-01-17 14:00:25 +0100

    create product class and methods

--- products.py
+++ products.py
@@ -1,32 +1,39 @@
+import database
+
+products_info = {'name': '<NAME>', 'price': '<price>', 'description': '<description>', 'vat': '<vat>',
+                 'inventory': '<inventory>', 'admin_id': '<admin_id>'}
+
+
 class AllProducts:
-    def __init__(self, id, name, price, description, vat, inventory, admin_id):
-        self.id = id
+    def __init__(self, name, price, description, vat, inventory, admin_id):
         self.name = name
-        self.price = float(price)
+        self.price = price
         self.description = description
-        self.vat = float(vat)
+        self.vat = vat
         self.inventory = inventory
         self.admin_id = admin_id
 
-    def add_new_product(self, name, price, description, vat, inventory, admin_id):
+    def add_new_product(self):
         run_new = True
         print("Please enter new product")
         while run_new:
-            product_name = input("Enter product name: ")
-            product_price = float(input("Enter product's price:"))
-            product_description = input("Enter product's description:")
-            product_vat = float(input("Enter product's vat"))
-            product_inventory = int(input("Enter product's inventory:"))
-            product_admin_id = input("Enter admin_id")
+            self.name = input("Enter product name: ")
+            self.price = float(input("Enter product's price:"))
+            self.description = input("Enter product's description:")
+            self.vat = float(input("Enter product's vat"))
+            self.inventory = int(input("Enter product's inventory:"))
+            self.admin_id = input("Enter admin_id")
             answer = input("Do you want to add another product? (y/n)")
             if answer == "y":
-                'db_E-handel'.add_new_product(name=product_name, price=product_price,
-                                              description=product_description, vat=product_vat,
-                                              inventory=product_inventory, admin_id=product_admin_id)
+                database.add_new_product(name=self.name, price=self.price, description=self.description,
+                                         vat=self.vat, inventory=self.inventory, admin_id=self.admin_id)
             elif answer == "n":
                 print("Exit")
                 run_new = False
         breakpoint()
 
 
-AllProducts.add_new_product()
+instance_of_products = AllProducts(name=products_info['name'], price=products_info['price'],
+                                   description=products_info['description'], vat=products_info['vat'],
+                                   inventory=products_info['inventory'], admin_id=products_info['admin_id'])
+instance_of_products.add_new_product()
